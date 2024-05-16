790a628b316e7c94bf3403a499b77d02a56b88b6
rose4725 <rose4725@gmail.com>
2024-05-16 17:12:58 +0900

    Lab1

--- lab0_c_asm_mix/lab0_c_asm_mix/SevenSeg.c
+++ lab0_c_asm_mix/lab0_c_asm_mix/SevenSeg.c
@@ -2,27 +2,15 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-void compare(int arr[])
+void compare()
 {
-	
-	int i, j, tmp;
-	/*
-	int arr[32] =
-			{ 2, 0, -7, -1, 3, 8, -4, 10
-			, -9, -16, 15, 13, 1, 4, -3, 14
-			, -8, -10, -15, 6, -13, -5, 9, 12
-			, -11, -14, -6, 11, 5, 7, -2, -12
-			};
-	*/
-	
-	for(int i = 0; i < 31; i++)
-	{
-		for(int j = 0; j< 31-i; j++)
-		{
-			tmp = arr[j];
-			arr[j] = arr[j+1];
-			arr[j+1] = tmp;	
-		}
-	};
-	
+	unsigned int* hex = (unsigned int*) 0xffff200c;
+	unsigned int* led = (unsigned int*) 0xffff2008;
+
+	hex[0] = 0x58;
+	hex[1] = 0x10;
+	hex[2] = 0x40;
+	hex[3] = 0x79;
+
+	*led = 0x1e0;
 }
